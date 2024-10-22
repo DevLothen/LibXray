95c60abab5bdf266c97a4f2584968683d20c50d3
DevLothen <52202168+DevLothen@users.noreply.github.com>
2024-10-22 13:19:44 +0200

    2.0.3

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
     ),
     .binaryTarget(
         name: "LibXray",
-	url: "https://github.com/DevLothen/LibXray/releases/download/2.0.0/LibXray.xcframework.zip",
-	checksum: "45cfb2a425b2b9f65ad65d7071cd111f5ab31944d7dd6776c28c7565561074eb"
+	url: "https://github.com/DevLothen/LibXray/releases/download/2.0.3/LibXray.xcframework.zip",
+	checksum: "e52fd922dc998e9fc972e8c139303d64755e4a912aef47c31510c2f28cfd794e"
     )
   ]
 )
